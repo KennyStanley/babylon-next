da4392dcfa7f2502baa088463fc78b3bcd4a372c
Kenny Stanley <kpstanleyjr@gmail.com>
2022-05-11 12:52:29 -0500

    feat: added tailwindcss

--- repos/web/pages/index.js
+++ repos/web/pages/index.js
@@ -3,15 +3,19 @@ import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 
 // Load Scene View on Demand
-const SceneView = dynamic(() => import(`../../client/views/SceneView`), {ssr: false})
+const SceneView = dynamic(() => import(`../../client/views/SceneView`), {
+    ssr: false,
+})
 
-export default function HomePage (props) {
-  const [loaded, setLoaded] = useState(false)
-  useEffect(() => setLoaded(true))
-  return <>
-    <Head>
-      <title>Babylon + Next.js Setup Successful!</title>
-    </Head>
-    {loaded && <SceneView {...props}/>}
-    </>
+export default function HomePage(props) {
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => setLoaded(true))
+    return (
+        <>
+            <Head>
+                <title>Babylon + Next.js Setup Successful!</title>
+            </Head>
+            {loaded && <SceneView {...props} />}
+        </>
+    )
 }
